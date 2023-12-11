b62b6d5596be435603c1a445b35581756d685aeb
Aleksandr Szestak <szestak@hotmail.com>
2023-12-11 23:26:43 +0300

    feat: implementation of burger menu

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -2,7 +2,7 @@
 let burger = document.querySelector(".burger");
 
 burger.addEventListener('click', ()=>{
-    document.querySelector('ul').classList.toggle('burger-menu-open');
+    document.querySelector('nav').classList.toggle('burger-menu-open');
     // document.querySelector('header').classList.toggle('burger-menu-open');
     document.querySelector('.burger').classList.toggle('burger-open');
 });
