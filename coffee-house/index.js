740b4efc22d01cfb03e3ec5fa766e3a71f57a261
Aleksandr-Szestak <szestak@hotmail.com>
2023-12-12 16:57:34 +0300

    fix: change burger-menu actions

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -3,6 +3,17 @@ let burger = document.querySelector(".burger");
 
 burger.addEventListener('click', ()=>{
     document.querySelector('nav').classList.toggle('burger-menu-open');
-    // document.querySelector('header').classList.toggle('burger-menu-open');
     document.querySelector('.burger').classList.toggle('burger-open');
+    document.querySelector('body').classList.toggle('burger-menu-open-y');
+});
+
+let linkMenu = document.querySelectorAll(".menu-link");
+
+linkMenu.forEach((hitMenu, idx) => {
+	hitMenu.addEventListener('click', () => {
+        document.querySelector('nav').classList.remove('burger-menu-open');
+        document.querySelector('nav').style.transition = '0s';
+        document.querySelector('.burger').classList.remove('burger-open');
+        document.querySelector('body').classList.remove('burger-menu-open-y');        
+	});
 });
